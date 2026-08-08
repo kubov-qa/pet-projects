d69d52a7c8a00b50567c23e45c29d02cb5634956
Mitia <kubov.work@gmail.com>
2026-08-08 13:26:31 +0300

    function price_after_downpayment is upgraded

--- sandbox.py
+++ sandbox.py
@@ -1,6 +1,11 @@
 MORTGAGE_MONTHS = 240 #используется в mortgage_per_month()
 MORTGAGE_SPREAD = 3.0 
 MEDIAN_COEFFICIENT = 93 / 129 # ~0.72 (коэффициент приведения средней ЗП к медиане)
+APARTMENT_SIZES_SQM = {
+    "one_room": 40,
+    "two_room": 55,
+}
+
 
 temp_rent_per_month = 39_000
 temp_price_per_meter = 215_000
@@ -11,12 +16,26 @@ temp_annual_rate_decimal = (temp_cbr_rate + MORTGAGE_SPREAD) / 100 #исполь
 temp_property_price = 10_000_000 #используется в mortgage_per_month()
 
 
-
+#аппрув но пойдет на доработку
 def mortgage_per_month(property_price, mortgage_percents, mortgage_months=240):
   i = mortgage_percents / 12
   numerator = i * (1 +i) ** mortgage_months
   denominator = (1 + i) ** mortgage_months - 1
   return property_price * numerator / denominator
-
 print(mortgage_per_month(temp_property_price, temp_annual_rate_decimal, MORTGAGE_MONTHS))
+
+#кандидат номер 1 на доработку
+def mortgage_affordability_index(price_per_sqm, salary):
+  apartment_price = price_per_sqm * 40
+  monthly_mortgage_payment = apartment_price / MORTGAGE_MONTHS
+  percentage_of_salary = (monthly_mortgage_payment / salary) * 100
+  return percentage_of_salary
+
+def price_after_downpayment(price, downpayment_percent=20):
+  return price * (1 - (downpayment_percent / 100))
+
+def calculate_apartment_price(price_per_sqm, square_meters=40):
+  return price_per_sqm * square_meters
+
+#calculate_apartment_price(temp_price_per_meter, APARTMENT_SIZES_SQM.get("one_room"))
 
--- to_do.py
+++ to_do.py
@@ -12,11 +12,6 @@ temp_total_mortage_percents = (temp_cbr_rate + MORTGAGE_SPREAD) / 100
 def apply_tax(before_tax):
   return before_tax * 0.87
 
-def remove_downpayment (price):
-  return price * 0.80
-
-def calculate_apartment_price(price_per_sqm, meters_qty=40):
-  return price_per_sqm * meters_qty
 
 def price_to_income_ratio(salary, property_price):
   return property_price / salary
@@ -27,17 +22,11 @@ def annual_rent_in_sqm(rent_price_1bedroom, price_per_sqm_secondary):
 def annual_salary_in_sqm(salary_average, price_per_sqm_secondary):
   return (salary_average * 12) / price_per_sqm_secondary
 
-def mortgage_affordability_index(price_per_sqm, salary):
-  apartment_price = price_per_sqm * 40
-  monthly_mortgage_payment = apartment_price / MORTGAGE_MONTHS
-  percentage_of_salary = (monthly_mortgage_payment / salary) * 100
-  return percentage_of_salary
    
 
 print(annual_rent_in_sqm(temp_rent_per_month, temp_price_per_meter))
 print(annual_salary_in_sqm(temp_salary, temp_price_per_meter))
 print(price_to_income_ratio(temp_salary, temp_property_price))
-print(mortgage_affordability_index(temp_price_per_meter, temp_salary))
 
 
 
