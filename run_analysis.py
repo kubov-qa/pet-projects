626a948c35cc278565b1fb1b4904654b93161ea0
Mitia <kubov.work@gmail.com>
2026-08-10 17:05:34 +0300

    temp values is removed

--- run_analysis.py
+++ run_analysis.py
@@ -1,8 +1,16 @@
+import json
 import housing_math as hm
 
-avg_salary_gross = 129_000
-avg_sqm_price = 215_000
-temp_cbr_rate = 14
+import os
+print(os.getcwd())
+
+with open("historical_data.json", "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+
+avg_salary_gross = data[-1].get("salary_average")
+avg_sqm_price = data[-1].get("price_per_sqm_secondary")
+temp_cbr_rate = data[-1].get("cbr_key_rate")
 temp_annual_rate_decimal = (temp_cbr_rate + hm.MORTGAGE_SPREAD) / 100
 
 median_salary_net = hm.convert_avg_to_median(hm.apply_tax(avg_salary_gross))
@@ -13,4 +21,5 @@ monthly_mortgage_payment = hm.mortgage_per_month(loan_amount, temp_annual_rate_d
 
 affordability_index = hm.mortgage_affordability_index(monthly_mortgage_payment, median_salary_net)
 
+print(monthly_mortgage_payment)
 print(affordability_index)
